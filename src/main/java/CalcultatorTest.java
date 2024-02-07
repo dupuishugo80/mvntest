0623f4f48c39ecc12778e8e18b368be43c75448c
W137025 <hugo.dupuis@worldline.com>
2024-02-07 09:33:40 +0100

    File

---
+++ src/main/java/CalcultatorTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTest {
+    
+    @Test
+    public void testSum() {
+        Calculator calculator = new Calculator();
+        int result = calculator.sum(3, 5);
+        assertEquals(8, result);
+    }
+}
